92c8c8b9267113f0d6caa928ee8b03a7dfccf343
Alex Fang <frjalex@temple.edu>
2017-02-18 18:07:56 -0500

    RooImageServer CLI skeleton

--- src/main/java/org/usfirst/frc/team4373/rooImageServer/RooImageServer.java
+++ src/main/java/org/usfirst/frc/team4373/rooImageServer/RooImageServer.java
@@ -6,11 +6,35 @@ package org.usfirst.frc.team4373.rooImageServer;
  */
 public class RooImageServer {
 
-    void initCam(int no) {
+    public static void runTests() {
+        System.out.println("initializing server...");
+        try {
 
+            RooUDPServer udpServer = new RooUDPServer(4373, 2048);
+            System.out.println("Server initialized. running...");
+            udpServer.run();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void runImageStream() {
+        // TODO
+    }
+
+    public static void showImageStream() {
+        // TODO
     }
 
     public static void main(String[] args) {
-        
+        // advent of a gigantic if...
+        if(args[1].equals("--runTests")) {
+            runTests();
+        } else if(args[1].equals("--runImageStream")) {
+            runImageStream();
+        } else if(args[1].equals("--showImageStream")) {
+            showImageStream();
+        }
     }
 }
