d34dcef6cca30e7e2532c0b280d7b56a72918b47
Alex Fang <frjalex@temple.edu>
2017-02-18 16:38:46 -0500

    Init RooUDPServer, RooSerializableImage

--- src/main/java/org/usfirst/frc/team4373/RooImageServer/RooImageServer.java
+++ src/main/java/org/usfirst/frc/team4373/RooImageServer/RooImageServer.java
@@ -1,10 +1,17 @@
 package org.usfirst.frc.team4373.RooImageServer;
 
+import org.usfirst.frc.team4373.roosight.RooCamera;
+
 /**
  * The main image server class.
  * @author aaplmath
  */
 public class RooImageServer {
+
+    void initCam(int no) {
+
+    }
+
     public static void main(String[] args) {
         
     }
